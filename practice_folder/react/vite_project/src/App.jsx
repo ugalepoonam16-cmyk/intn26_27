994c99a649610d171ff94c15274f5b13d807b829
ugalepoonam-cmyk <ugalepoonam16@gmail.com>
2026-06-23 10:30:02 +0530

    fake API fetch

---
+++ practice_folder/react/vite_project/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Layout from "./Layout/Layout";
+import Home from "./Pages/Home";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
